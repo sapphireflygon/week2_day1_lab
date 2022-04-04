8bfaf09f96ce0473a627b8c8b40793745ff6a5d3
Jennifer Lane <jennifer.lane610@gmail.com>
2022-04-04 14:37:58 +0100

    added comment on has_player() with more concise  solution from instructors

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -14,6 +14,9 @@ class Team:
             if find_player == player:
                 found_player = True
         return found_player
+    # more concise way of writing this: 
+    # def has_player(self, player):
+        # return self.players.count(player) > 0   # will automatically return boolean
 
     def play_game(self, win_game):
         if win_game:
